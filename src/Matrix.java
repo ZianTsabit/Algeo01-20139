525f58fdfde420a5abc06005b5073d73dfef4d6c
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-09-17 10:22:41 +0700

    Membuat konstruktor

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,18 +1,26 @@
-
-
-
-
-
-
 class Matrix {
     
-    /*  ===CONSTRUCTOR=== 
-    - MakeMatriks
+    // Atribut
+    int[][] Mat = new int[10][10];
+
+    /* ===CONSTRUCTOR=== */
+    Matrix() {
+        int i,j;
+        for (i=0; i<10; i++) {
+            for (j=0; j<10; j++) {
+                this.Mat[i][j] = 0;
+            }
+        }
+    }
+
+    void makeMatriks(Matrix M) {
+        M = new Matrix();
+    }
     
 
 
 
-        ===SELEKTOR===
+    /*    ===SELEKTOR===
     - getBaris
     - getKolom
     - setBaris
