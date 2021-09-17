78a5e1bb34e30feb11303f42cae0337472538ef4
ghaziankamil <16520465@std.stei.itb.ac.id>
2021-09-17 09:01:09 +0700

    ngelist fungsi apa aja yang kira kira dibutuhin

---
+++ src/Matrix.java
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+class Matrix {
+    
+    /*  ===CONSTRUCTOR=== 
+    - MakeMatriks
+    
+
+
+
+        ===SELEKTOR===
+    - getBaris
+    - getKolom
+    - setBaris
+    - setKolom
+
+        ===INPUT ATAU OUTPUT ===
+    - BacaMatriks
+    - TulisMatriks
+
+        ===FUNGSI HELPER===
+    - tukarBaris
+    - tambahBaris
+    - kaliBaris
+    - 
+
+        ===FUNGSI - FUNGSI===
+    - determinan
+    - inversMatrix
+    - gauss
+    - gaussJordan
+
+
+
+
+    */
+
+
+
+
+
+
+}
