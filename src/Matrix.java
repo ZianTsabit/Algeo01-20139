151a72eb33575a9f972825c73df946d42b4830f5
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-09-17 17:28:07 +0700

    Update konstruktor

--- src/Matrix.java
+++ src/Matrix.java
@@ -18,11 +18,6 @@ class Matrix {
         }
     }
 
-    void makeMatriks(Matrix M) {
-    // Membuat matriks M
-	M = new Matrix();
-    }
-
     /* ===SELEKTOR=== */
     void getBaris(int i) {
     // Mengeluarkan baris Matrix M dengan indeks ke-i
