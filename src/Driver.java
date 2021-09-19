78658816a48f3943f75fa254a74829db4881957b
ghaziankamil <16520465@std.stei.itb.ac.id>
2021-09-20 06:06:27 +0700

    garap driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,17 @@
-public class Driver {
+import java.util.*;
+
+class Driver {
+
+    public static void main(String[] args){
+
+        //Matriks buat ngetest
+        
+        double mat[][] = { { 3.0, 2.0, -4.0, 3.0 },
+        { 2.0, 3.0, 3.0, 15.0 },
+        { 5.0, -3, 1.0, 14.0 } };
+
+        
+    }
     
+
 }
