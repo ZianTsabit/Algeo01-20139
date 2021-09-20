5faac368a204768a0a4e602b1f4c7bbc45fab6c5
Willy Wilsen <13520160@std.stei.itb.ac.id>
2021-09-20 16:18:21 +0700

    Tes driver

---
+++ src/DriverMatrix.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class DriverMatrix {
+    public static void main(String[] args) {
+	Scanner in = new Scanner (System.in);
+
+	// masukan baris dan kolom yang diinginkan
+	System.out.print("Masukan baris: ");
+	int Bar = in.nextInt();
+	System.out.print("Masukan kolom: ");
+	int Kol = in.nextInt();
+
+	// membuat objek matriks
+	Matrix M = new Matrix(Bar,Kol);
+
+	// mengisi matriks
+	M.bacaMatriks();
+
+	// menulis matriks
+	M.tulisMatriks();
+
+	double M1[][];
+	M1 = new double[Bar][Kol];
+	M.reduceMatriks(M1,0,0);
+	for (int i =0; i < Bar-1; i++) {
+		for (int j = 0; j < Kol-1; j++) {
+			System.out.print(M1[i][j]+ " ");
+		}
+		System.out.println();
+	}
+	
+	double det = M.determinanSarrus(M1,Bar-1,Kol-1);
+	System.out.print(det);
+    }
+}
